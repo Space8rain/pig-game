161cd97ae03066d80e16c292e7138cf7a8105617
niNovikov <gambitrus38@yandex.ru>
2023-05-29 22:23:41 +0300

    add markdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,47 @@
-import './App.scss';
+import "./App.scss";
+import Dice from './dice/Dice';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <div className="game">
 
-      </header>
+          <div className="players">
+
+            <div className="player">
+              <h1 className="player_name">Player 1</h1>
+              <h2 className="player_current">0</h2>
+              <div className="player_score">
+                <p>Score</p>
+                <p>0</p>
+              </div>
+            </div>
+
+            <div className="player">
+              <h1 className="player_name">Player 2</h1>
+              <h2 className="player_current">0</h2>
+              <div className="player_score">
+                <p>Score</p>
+                <p>0</p>
+              </div>
+            </div>
+
+            <div className="player">
+              <h1 className="player_name">Player 2</h1>
+              <h2 className="player_current">0</h2>
+              <div className="player_score">
+                <p>Score</p>
+                <p>0</p>
+              </div>
+            </div>
+            
+          </div>
+          <Dice />
+            <div id="roll" className="roll_button">
+              <button>Roll dice!</button>
+            </div>
+
+        </div>
     </div>
   );
 }
